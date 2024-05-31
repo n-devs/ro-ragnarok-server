58a5da372f462f6b5a667b0920fcdb2d28f0b323
idev-coder <idevcoder7@gmail.com>
2024-05-31 18:32:39 +0700

    update

---
+++ server/emulador/src/custom/defines_pre.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) rAthena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef CONFIG_CUSTOM_DEFINES_PRE_HPP
+#define CONFIG_CUSTOM_DEFINES_PRE_HPP
+
+/**
+ * rAthena configuration file (http://rathena.org)
+ * For detailed guidance on these check http://rathena.org/wiki/SRC/config/
+ **/
+
+#define PACKET_OBFUSCATION_KEY1 0x290551EA
+#define PACKET_OBFUSCATION_KEY2 0x2B952C75
+#define PACKET_OBFUSCATION_KEY3 0x2D67669B
+#define PRERE
+
+
+#endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
